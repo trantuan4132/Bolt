e27d63c5e1425a5e19c6c04695eafef49061ab91
trantuan4132 <trantuan4132@gmail.com>
2021-01-17 20:20:05 +0700

    Add files via upload

--- KaggleCommands.py
+++ KaggleCommands.py
@@ -31,31 +31,34 @@ class KaggleCommands(commands.Cog, name = "Kaggle Commands"):
         self.bot = bot
 
     @commands.command(name='kaggle', help = 'List all certificate from user')
-    async def kaggle(self, ctx, username):
-        url = 'https://www.kaggle.com/learn/certification/'
-        count = 0
-        cert_lst = []
+    async def kaggle(self, ctx, username='/'):
+        try:
+            profile = requests.get('https://www.kaggle.com/' + username)
+            soup = BeautifulSoup(profile.text, "html.parser")
+            attrs = soup.find_all('script', attrs={'class' : 'kaggle-component'})[1]
+            img_pro_url = json.loads(re.findall('Kaggle.State.push\((.*?)\);', str(attrs))[0])['userAvatarUrl']
 
-        for course in self.courses:
-            r = requests.get(url + username + '/' + course)
-            soup = BeautifulSoup(r.text, "html.parser")
-            img = 'No image found'
-            try:
-                img_url = soup.find('meta', attrs={'property' : 'og:image'})['content']
-                # img = Image.open(urlopen(img_url))
-                cert_lst.append(course.replace('-', ' ').title())
-                count += 1
-            except:
-                # print(img)
-                continue
+            url = 'https://www.kaggle.com/learn/certification/'
+            count = 0
+            cert_lst = []
 
-        profile = requests.get('https://www.kaggle.com/' + username)
-        soup = BeautifulSoup(profile.text, "html.parser")
-        attrs = soup.find_all('script', attrs={'class' : 'kaggle-component'})[1]
-        img_pro_url = json.loads(re.findall('Kaggle.State.push\((.*?)\);', str(attrs))[0])['userAvatarUrl']
+            for course in self.courses:
+                r = requests.get(url + username + '/' + course)
+                soup = BeautifulSoup(r.text, "html.parser")
+                img = 'No image found'
+                try:
+                    img_url = soup.find('meta', attrs={'property' : 'og:image'})['content']
+                    # img = Image.open(urlopen(img_url))
+                    cert_lst.append(course.replace('-', ' ').title())
+                    count += 1
+                except:
+                    # print(img)
+                    continue
 
-        embed = discord.Embed(title="Username: ", description=username, color=0x00ff00)
-        embed.set_thumbnail(url=img_pro_url)
-        embed.add_field(name="Total certificates:", value=count, inline=False)
-        embed.add_field(name="List of certificates:", value='\n'.join(cert_lst) or 'No certificate found', inline=False)
-        await ctx.send(embed=embed)
+            embed = discord.Embed(title="Username: ", description=username, color=0x04d9ff)
+            embed.set_thumbnail(url=img_pro_url)
+            embed.add_field(name="Total certificates:", value=count, inline=False)
+            embed.add_field(name="List of certificates:", value='\n'.join(cert_lst) or 'No certificate found', inline=False)
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send('Username not found')
